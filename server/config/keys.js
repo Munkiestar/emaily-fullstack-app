63c4794bbc883193f069b3f31f4724b0db2abc46
Munkiestar <munkieryder@gmail.com>
2023-06-28 21:16:56 +0200

    add/google oauth passport

---
+++ server/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  googleClientID:
+    "499407780313-f2n7fa6dm7pkr7j7cde7gbk480da2gbo.apps.googleusercontent.com",
+  googleClientSecret: "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",
+};
--- server/index.js
+++ server/index.js
@@ -1,13 +1,27 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const { googleClientID, googleClientSecret } = require("./config/keys");
 
 const app = express();
 const PORT = 5000;
 console.log("app: ", app);
 
 //  route handlers
-app.get("/", (req, res) => {
-  res.send({ hi: "railway" });
-});
+
+//  passport auth
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: googleClientID,
+      clientSecret: googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken) => {
+      console.log("accessToken: ", accessToken);
+    }
+  )
+);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
