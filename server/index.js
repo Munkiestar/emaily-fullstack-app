10a2cdd94d014b8efcefa49c23eb1ddb5e1a878d
Munkiestar <munkieryder@gmail.com>
2023-07-04 19:12:59 +0200

    add passport and redirect back, nodemon

--- server/index.js
+++ server/index.js
@@ -7,8 +7,6 @@ const app = express();
 const PORT = 5000;
 console.log("app: ", app);
 
-//  route handlers
-
 //  passport auth
 passport.use(
   new GoogleStrategy(
@@ -17,11 +15,24 @@ passport.use(
       clientSecret: googleClientSecret,
       callbackURL: "/auth/google/callback",
     },
-    (accessToken) => {
+    (accessToken, refreshToken, profile, done) => {
       console.log("accessToken: ", accessToken);
+      console.log("refreshToken: ", refreshToken);
+      console.log("profile: ", profile);
+      // console.log("done: ", done);
     }
   )
 );
+
+//  route handlers
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
