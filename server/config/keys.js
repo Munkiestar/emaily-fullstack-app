f453e2b637bf8cb48cddef6c1e91c862fa063b41
Munkiestar <munkieryder@gmail.com>
2023-07-08 11:56:19 +0200

    add mongoose, untrack config files

--- server/config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  googleClientID:
-    "499407780313-f2n7fa6dm7pkr7j7cde7gbk480da2gbo.apps.googleusercontent.com",
-  googleClientSecret: "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",
-};
