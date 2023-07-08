38d131e65d31dce84fdc33f66a7c516511a2efae
Munkiestar <munkieryder@gmail.com>
2023-07-08 10:04:09 +0200

    refactor/server folder structure

--- server/index.js
+++ server/index.js
@@ -1,38 +1,13 @@
 const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const { googleClientID, googleClientSecret } = require("./config/keys");
+require("./services/passport");
 
 const app = express();
 const PORT = 5000;
-console.log("app: ", app);
 
 //  passport auth
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: googleClientID,
-      clientSecret: googleClientSecret,
-      callbackURL: "/auth/google/callback",
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log("accessToken: ", accessToken);
-      console.log("refreshToken: ", refreshToken);
-      console.log("profile: ", profile);
-      // console.log("done: ", done);
-    }
-  )
-);
 
 //  route handlers
-app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"],
-  })
-);
-
-app.get("/auth/google/callback", passport.authenticate("google"));
+require("./routes/authRoutes")(app);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
