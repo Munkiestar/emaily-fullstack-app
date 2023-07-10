57a8fc65a2a038870914bc21f9617ff1dbae3710
Munkiestar <munkieryder@gmail.com>
2023-07-10 18:38:07 +0200

    add/cookie session

--- server/index.js
+++ server/index.js
@@ -1,18 +1,30 @@
 const express = require("express");
-const { mongoURI } = require("./config/keys");
+const { mongoURI, cookieKey } = require("./config/keys");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 
 // executing files
 require("./models/User");
 require("./services/passport");
 
+const PORT = 5000;
+
 // connect to db
 mongoose.connect(mongoURI);
 
 const app = express();
-const PORT = 5000;
+app.use(
+  cookieSession({
+    // 30 days
+    maxAge: 30 * 60 * 60 * 24 * 1000,
+    keys: [cookieKey],
+  })
+);
 
-//  passport auth
+// tell passport to use cookies for auth
+app.use(passport.initialize());
+app.use(passport.session());
 
 //  route handlers
 require("./routes/authRoutes")(app);
