cb521646b8bfcb984e47dc903898aab2ea641c1d
Munkiestar <munkieryder@gmail.com>
2023-07-11 18:27:47 +0200

    add/dev and prod env keys

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -22,3 +22,12 @@ module.exports = (app) => {
     res.send(req.user);
   });
 };
+// munkie-prod
+// AOc3YoR5ITybTR7J
+// mongodb+srv://munkie-prod:<password>@emailyprod.fs12cgj.mongodb.net/?retryWrites=true&w=majority
+
+// google api
+// Client ID
+//871264525946-89i3b31uukqs9aind5e2ge0adhg81mt1.apps.googleusercontent.com
+// Client secret
+//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
