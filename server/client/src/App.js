758bbb433b13b7e4f66862125964281d067ecd52
Munkiestar <munkieryder@gmail.com>
2023-07-16 22:05:36 +0200

    add/initial configuration for routes

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,9 +1,22 @@
 import "./app.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 function App() {
   return (
     <div className="app">
-      <h1>refactor react</h1>
+      <Router>
+        <div>
+          <Header />
+          <Route path="/" component={Landing} exact />
+          <Route path="/surveys" component={Dashboard} exact />
+          <Route path="/surveys/new" component={SurveyNew} exact />
+        </div>
+      </Router>
       <a href="/auth/google">Sign in</a>
     </div>
   );
