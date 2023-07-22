e98b1afc3664d2a2278c53379d6f8dc021c1cd1f
Munkiestar <munkieryder@gmail.com>
2023-07-22 13:37:29 +0200

    finishing FE payment stuff

--- server/client/src/components/Payments.js
+++ server/client/src/components/Payments.js
@@ -1,18 +1,24 @@
 import React from "react";
 
 import StripeCheckout from "react-stripe-checkout";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
-const Payments = () => {
+const Payments = ({ handleToken }) => {
   return (
     <StripeCheckout
+      name="Emaily"
+      description="$5 for 5 email credits"
       amount={500}
-      token={(token) => console.log("token:", token)}
+      token={(token) => handleToken(token)}
       stripeKey={
         process.env.REACT_APP_STRIPE_KEY ||
         "pk_test_51NVHvcIK5vxPgzzPSkrOgH4Z27e64VNtVfaYLuv4tnLJ8XXiMmi7MJts8B7nrC7iuWazY3gTqs5kpxLFmu4qQyM000fpOucpKl"
       }
-    />
+    >
+      <div className="btn">Add Credits</div>
+    </StripeCheckout>
   );
 };
 
-export default Payments;
+export default connect(null, actions)(Payments);
