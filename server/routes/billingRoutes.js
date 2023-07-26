494adaae19c03c00b8854e77392858b31a8bc11e
Munkiestar <munkieryder@gmail.com>
2023-07-26 17:47:47 +0200

    creating successful stripe charge

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -5,5 +5,18 @@ const stripe = require("stripe")(stripeSecretKey);
 console.log("stripekey: ", stripe);
 
 module.exports = (app) => {
-  app.post("/api/stripe", (req, res) => {});
+  // create billing i.e. credit card charge
+  app.post("/api/stripe", async (req, res) => {
+    // console.log("req.body: ", req.body);
+    const { id } = req?.body;
+
+    const charge = await stripe.charges.create({
+      amount: 500,
+      currency: "usd",
+      description: "$5 for 5 Credits",
+      source: id,
+    });
+
+    // console.log("charge: ", charge);
+  });
 };
