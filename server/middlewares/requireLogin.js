c2377dd1b662cd10bd82179fbd6ea2fd06dc9eee
Munkiestar <munkieryder@gmail.com>
2023-07-26 18:19:03 +0200

    add/stripe check login middleware

---
+++ server/middlewares/requireLogin.js
@@ -0,0 +1,6 @@
+module.exports = (req, res, next) => {
+  // check if user is signed in
+  if (!req.user) return res.status(401).send({ error: "You must log in" });
+
+  next();
+};
--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -2,11 +2,12 @@ const { stripeSecretKey } = require("../config/keys");
 
 const stripe = require("stripe")(stripeSecretKey);
 
-console.log("stripekey: ", stripe);
+// middleware
+const requireLogin = require("../../server/middlewares/requireLogin");
 
 module.exports = (app) => {
   // create billing i.e. credit card charge
-  app.post("/api/stripe", async (req, res) => {
+  app.post("/api/stripe", requireLogin, async (req, res) => {
     // console.log("req.body: ", req.body);
     const { id } = req?.body;
 
@@ -18,5 +19,14 @@ module.exports = (app) => {
     });
 
     // console.log("charge: ", charge);
+    if (charge.status !== "succeeded")
+      return res.status(402).json({ error: "Payment failed" });
+
+    // access to user and add credit
+    req.user.credits += 5;
+    const user = await req.user.save();
+
+    // send data to browser
+    res.send(user);
   });
 };
