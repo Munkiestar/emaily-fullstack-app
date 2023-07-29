98c94abf0a15f431ab27b1c903f68871f9e072b8
Munkiestar <munkieryder@gmail.com>
2023-07-29 16:04:47 +0200

    add check credit middleware

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,8 +1,8 @@
 const requireLogin = require("../middlewares/requireLogin");
+const checkCredit = require("../middlewares/checkCredit");
 
 module.exports = (app) => {
-  app.post("/api/surveys", requireLogin, (req, res) => {
-    // first make sure that user is logged in
-    // second check if user has enough money to send a survery
-  });
+  // first make sure that user is logged in
+  // second check if user has enough money to send a survey
+  app.post("/api/surveys", requireLogin, checkCredit, (req, res) => {});
 };
